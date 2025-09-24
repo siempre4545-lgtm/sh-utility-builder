2ac6d4ed12020102687866e941c92cde4567468f
김상호 <siempre4545@naver.com>
2025-09-24 18:54:44 +0900

    feat: AdSense 자동광고 반영으로 수동 광고 배너 제거
    - 메인페이지에서 AdBanner 컴포넌트 제거
    - AdSense 자동광고와 충돌 방지
    - 개발모드 광고영역 표시 해결

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from '@/components/Hero'
 import UtilityGrid from '@/components/UtilityGrid'
-import AdBanner from '@/components/AdBanner'
 
 // 캐싱 비활성화 - 실시간 업데이트 보장
 export const dynamic = 'force-dynamic'
@@ -10,9 +9,7 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <AdBanner position="top" size="medium" />
       <UtilityGrid />
-      <AdBanner position="bottom" size="medium" />
     </>
   )
 }
